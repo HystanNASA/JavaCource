3660bd32cbbc9452ad6689bed19e1158d04ba0f2
SannikovRoman <kenobi-2002@mail.ru>
2021-09-29 21:27:25 +0300

    lab14 was added

---
+++ Lab1.4/src/hystannasa/mirea/lab14/Main.java
@@ -0,0 +1,8 @@
+package hystannasa.mirea.lab14;
+
+public class Main {
+    public static void main(String[] args) {
+        Window window = new Window();
+        window.setVisible(true);
+    }
+}
