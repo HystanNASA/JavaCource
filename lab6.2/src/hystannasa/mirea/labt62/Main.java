f17cf0412f9fb3513dfb4da75124a4deb052422d
SannikovRoman <kenobi-2002@mail.ru>
2021-11-24 12:42:00 +0300

    lab 6.2

---
+++ lab6.2/src/hystannasa/mirea/labt62/Main.java
@@ -0,0 +1,16 @@
+package hystannasa.mirea.labt62;
+
+public class Main {
+    public static void main(String[] args){
+        MyArrayList arr = new MyArrayList();
+        arr.add('a');
+        arr.add('b');
+        arr.add('c');
+        arr.add('d');
+        System.out.println("Data: " + arr);
+        System.out.println("Size: " + arr.size());
+        System.out.println("Is empty? " + arr.isEmpty());
+        arr.clear();
+        System.out.println("Data: " + arr);
+    }
+}
